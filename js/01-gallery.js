2b614cc1b4f5877a34834fdae4ac4774e0572597
VolodymyrPakhomov <vova.zest997@gmail.com>
2022-09-28 02:11:01 +0300

    27.09
    27.09

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,67 +1,55 @@
-
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
 
-//Створення і рендер розмітки на підставі масиву даних 
-//galleryItems і наданого шаблону елемента галереї.
+const galleryBoxMarkup = document.querySelector(".gallery");
 
-const makeGalleryItemsMarkup = (galleryitems) =>
-    galleryitems.map(({ preview, original, description }) =>
-    `<div class="gallery__item"> 
-    <a class="gallery__link" href="${preview}">
-        <img class ="gallery__image"
-            src = "${preview}" data-source = "${original}"   
-            alt = "${description}" 
-            width = "100%vw" height = "100%vh"
+function createGallery(galleryItems) {
+    return galleryItems.map(({preview, original, description}) =>
+    `<div class = "gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
             />
-            </a>
-    </div>`).join("");
-
-    
-const galleryContainer = document.querySelector(".gallery");
-const itemsMarkup = makeGalleryItemsMarkup(galleryItems);
-
-galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
-
-//Реалізація делегування на div.gallery і 
-//отримання url великого зображення.
-
-galleryContainer.addEventListener('click', onGalleryContainerClick);
-
-function onGalleryContainerClick(evt) {
-    evt.preventDefault();
-    const imageEl = evt.target;
-  //const isImageEl = imageEl.classList.contains('gallery__image');
-  //if (!isImageEl) { return; };
-    
-  //Відкриття модального вікна по кліку на елементі галереї. 
-    
-    const doBigImageEl = imageEl.dataset.source;
-    
-  //Заміна значення атрибута src елемента <img> 
-  //в модальному вікні перед відкриттям.
-
-    const modal = basicLightbox.create(`
-    <img src = "${doBigImageEl}"  width = "100%" height = "100%"/>
-`, {
-    onShow: modal => {
-    window.addEventListener('keydown', onEscKeyPress);
-    
-    },
-    onClose: modal => {
-        indow.removeEventListener('keydown', onEscKeyPress);
-
-    },
-    }
-    );
-    modal.show();
-
-    function onEscKeyPress(evt) {
-    const ECS_KEY_CODE = 'Escape';
-    if (evt.code === ECS_KEY_CODE) {
-    modal.close();
-    }
-    };
+        </a>
+    </div>`)
+        .join("");
 }
+
+
+
+const addGalleryMarkup = createGallery(galleryItems);
+
+galleryBoxMarkup.innerHTML = addGalleryMarkup;
+
+galleryBoxMarkup.addEventListener("click", onImageClick);
+
+
+function onImageClick(event) {
+    // запрет откр ссылки на картинку
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+//  использ ligthbox
+    
+    const instance = basicLightbox.create(`<div class="modal"><img 
+        src="${event.target.dataset.source}" 
+        width="800"
+        heigth = "600"></div>`);
+    instance.show();
+
+    galleryBox.addEventListener("keydown", (event) => {
+    
+    if (event.code === "Escape") {
+        instance.close();
+    }
+})
+}
+
+
+console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,66 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const galleryBoxMarkup = document.querySelector(".gallery");
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({preview, original, description}) =>
+    `<div class = "gallery__item">
+        <a class="gallery__item" 
+            href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>
+    </div>`)
+        .join("");
+}
+
+
+
+const addGalleryMarkup = createGallery(galleryItems);
+
+galleryBoxMarkup.innerHTML = addGalleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+
+    captionDelay:250,
+    captionsData:"alt",
+    });
+
+// let gallery = new SimpleLightbox('.gallery a', { 
+
+//     captions: true,
+//      captionPosition: 'bottom',
+//      captionDelay:250,
+//     captionsData:"alt"});
+
+// gallery.on('show.simplelightbox');
+
+
 console.log(galleryItems);
+// galleryBoxMarkup.addEventListener("click", onImageClick);
 
-const makeGalleryItemsMarkup = (galleryitems) =>
-    galleryitems.map(({ preview, original, description }) =>
-    ` <a class="gallery__item" href="${original}">
-        <img class ="gallery__image"
-            src = "${preview}"    
-            alt = "${description}" 
-            "/>
-            </a>
-    </div>`).join("");
+// function onImageClick(event) {
+//     // запрет откр ссылки на картинку
+//     event.preventDefault();
+//     if (event.target.nodeName !== "IMG") {
+//         return;
+//     }
+// //  использ ligthbox
+//     const instance = basicLightbox.create(`<div class="modal"><img 
+//         src="${event.target.dataset.source}" 
+//         width="800"
+//         heigth = "600"></div>`);
+//     instance.show();
+//     galleryBox.addEventListener("keydown", (event) => {
+//     if (event.code === "Escape") {
+//         instance.close();
+//     }
+// })
+// }
 
-const galleryContainer = document.querySelector(".gallery");
-const itemsMarkup = makeGalleryItemsMarkup(galleryItems);
 
-galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
-//galleryContainer.addEventListener('click', onGalleryContainerClick);
-//function onGalleryContainerClick(evt) {
-  //evt.preventDefault();
-        
-    const anim = new SimpleLightbox('.gallery a', {
-    captions: true,
-    captionsData: 'alt',
-    captionDelay: 250,
-}); 
-//}
